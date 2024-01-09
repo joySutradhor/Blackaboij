31b914d7adb42091c4b30d3f9c77063401c13d75
joySutradhor <joysutradhorcmt@gmail.com>
2024-01-09 15:35:58 +0600

    button space decrease

--- src/ClientSide/Home/BlackSaleSection/BlackSaleSection.jsx
+++ src/ClientSide/Home/BlackSaleSection/BlackSaleSection.jsx
@@ -14,7 +14,7 @@ const BlackSaleSection = () => {
             <div className="flex flex-col text-white justify-center items-center h-full">
                 <h3 className="text-[12px] md:text-[16px]">UP TO 60% OFF ON SELECTED ITEMS</h3>
                 <h2 className= "md:text-[110px] text-[30px]">Black Friday Sale</h2> 
-                <h4 className="py-10"><button className="bg-black md:py-5 md:px-10 px-4 py-2 text-[12px] md:text-[16px] rounded-[5px] hover:text-[#b1b1b1]">Shop Now</button></h4>
+                <h4 className="md:py-10 py-3"><button className="bg-black md:py-5 md:px-10 px-4 py-2 text-[12px] md:text-[16px] rounded-[5px] hover:text-[#b1b1b1]">Shop Now</button></h4>
                 
             </div>
         </div>
