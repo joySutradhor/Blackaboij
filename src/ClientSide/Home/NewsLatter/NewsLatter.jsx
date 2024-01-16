32364df02b72126ebded2f6e3157d64e4c68dec2
joySutradhor <joysutradhorcmt@gmail.com>
2024-01-16 15:43:33 +0600

    newslatter button centerd

--- src/ClientSide/Home/NewsLatter/NewsLatter.jsx
+++ src/ClientSide/Home/NewsLatter/NewsLatter.jsx
@@ -6,8 +6,8 @@ const NewsLatter = () => {
         <div className=" md:h-[300px]  mb-5 md:mb-[50px] bg-slate-200 flex flex-col justify-center items-center " >
             <h1 className=" text-center font-custom font-bold pt-4 md:pt-0">News Latter</h1>
             <p className="text-center text-[#646464] md:text-[16px] text-[12px] px-5 md:px-0">Subscribe to receive updates, access to exclusive deals, and more.</p>
-            <div className="flex md:flex-row flex-col gap-4 md:py-8 py-4">
-                <input type="email" placeholder="Email Address" className="border border-[#b1b1b1]  pl-2 bg-transparent focus:outline-none text-gray-600 w-[250px] "></input>
+            <div className="flex md:flex-row flex-col justify-center items-center  gap-4 md:py-8 py-4">
+                <input type="email" placeholder="Email Address" className="border md:text-[15px] text-[12px] border-[#b1b1b1]  pl-2 bg-transparent focus:outline-none text-gray-600 w-[250px] "></input>
                 <Button buttonText="SUBSCRIBE" ></Button>
             </div>
         </div>
